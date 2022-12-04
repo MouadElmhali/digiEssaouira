79b02192f0bfb0daed8b8a26e74dcbed750749db
MouadElmhali <mouadelmhali@gmail.com>
2022-12-04 21:46:10 +0100

    some fixes 3

--- apolloClient.ts
+++ apolloClient.ts
@@ -47,7 +47,7 @@ function createApolloClient() {
   });
 
   const httpLink = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+    uri: "http://localhost:3000/api/graphql",
     credentials: "same-origin",
   });
 
