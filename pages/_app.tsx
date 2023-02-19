73cd5ab9f57a0a4e07b16c52438b413368565969
@mouadelmhali <mouadelmhali@gmail.com>
2023-02-19 23:14:39 +0100

    remove side bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,14 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { ApolloProvider, useApolloClient } from "@apollo/client";
 import { useApollo } from "../apolloClient";
-import SideBar from "../components/sideBar";
 import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps);
   const router = useRouter();
-  const showHeader =
-    router.pathname === "/courses/[courseName]/course" ? false : true;
   return (
     <ApolloProvider client={client}>
       <NavBar />
-      {showHeader && <SideBar />}
       <Component {...pageProps} />
       <Footer />
     </ApolloProvider>
