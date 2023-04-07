6b93efbd2a09ef827c1b82096e8d08154dcb458d
ismail hamdach <ismailhamdach@gmail.com>
2023-04-07 23:35:40 +0000

    fix header space between

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -30,21 +30,14 @@ export default function Header({
       :
       (
         <header
-        className="flex flex-wrap items-center justify-between px-10 mt-36"
+        className="flex flex-wrap md:flex-nowrap items-center justify-center px-10 mt-36"
         >
-          {/* <div className="absolute pt-14 md:pt-auto top-44 -translate-y-1/2  right-1/2 translate-x-1/2 flex flex-col  items-center w-11/12 [&>p:first-child]:text-center">
-          <div className="flex items-center justify-center flex-wrap md:flex-nowrap">
-            <p className="w-full md:w-3/6 text-center md:text-auto md:text-right text-blue font-bold text-5xl ">{title}</p>
-            <img src={image} alt="" className="w-full md:w-2/6" />
-          </div>
-        </div> */}
+           <h1 className="text-4xl font-bold mb-4 text-left text-primary sm:text-center">{title}</h1>
+      
 
-          <div className="w-full lg:w-1/2">
-              <h1 className="text-4xl font-bold mb-4 text-center text-primary sm:text-center">{title}</h1>
-            </div>
-            <div className="w-full lg:w-1/2 mb-12 lg:mb-0 lg:pr-16">
-              <img className="w-full h-auto max-w-md" src={image} alt="Placeholder image"/>
-            </div>
+          <div className="w-full lg:w-1/2 mb-12 lg:mb-0 lg:pr-16">
+            <img className="w-full h-auto max-w-md" src={image} alt="Placeholder image"/>
+          </div>
            
 
         </header>
