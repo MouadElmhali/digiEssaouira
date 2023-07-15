e57da9bd61a6d03a87b8c0aca1a4ab13b3af1d62
ismail hamdach <ismailhamdach@gmail.com>
2023-07-15 20:29:15 +0100

    Add rewrites

--- next.config.js
+++ next.config.js
@@ -9,6 +9,22 @@ const nextConfig = {
     config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*'
+      },
+      {
+        source: '/',
+        destination: 'http://localhost:3000/'
+      },
+      {
+        source: '/:path*',
+        destination: 'http://localhost:3000/:path*'
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
