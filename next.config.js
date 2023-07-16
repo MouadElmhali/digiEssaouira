b7107236fd32dac74fa7ddfd0ef1b0207045d2db
ismail hamdach <ismailhamdach@gmail.com>
2023-07-16 21:35:04 +0100

    Add rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*'
+      },
+      {
+        source: '/blog',
+        destination: 'http://localhost:3001/blog'
+      },
+      {
+        source: '/blog/:path*',
+        destination: 'http://localhost:3001/blog/:path*'
+      }
+    ]
+  },
   swcMinify: true,
   typescript: {
     ignoreBuildErrors: true,
@@ -9,22 +25,6 @@ const nextConfig = {
     config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/:path*'
-      },
-      {
-        source: '/',
-        destination: 'http://localhost:3000/'
-      },
-      {
-        source: '/:path*',
-        destination: 'http://localhost:3000/:path*'
-      }
-    ]
-  }
 };
 
 module.exports = nextConfig;
