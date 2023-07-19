36e1d48d3c002725ce319ac5ce5c08ac6a6c25b4
ismail hamdach <ismailhamdach@gmail.com>
2023-07-19 20:54:59 +0100

    Contact us waiting effect

--- pages/contactUs/index.tsx
+++ pages/contactUs/index.tsx
@@ -1,10 +1,11 @@
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, useFormikContext } from "formik";
 import * as Yup from "yup";
 import Head from "next/head";
 import {sendContactForm} from "../../lib/api";
 
 import { useMutation } from "@apollo/client";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 export default function ContactUs(): JSX.Element {
   const ContactSchema = Yup.object().shape({
@@ -16,17 +17,22 @@ export default function ContactUs(): JSX.Element {
     message: Yup.string().required("هذه الخانة مطلوبه"),
   });
 
+  // const {isSubmitting, setIsSubmitting} = useState(false);
+
+
+  // const { submitForm  } = useFormikContext();
+
   // const [SendContactForm, { loading, data }] = useMutation(sendContactForm);
 
   return (
     <>
       <Head>
-        <title>DigiEssaouira | أشارك تجربتي</title>
+        <title>DigiEssaouira | تواصلو معنا</title>
       </Head>
       <div className="mt-48 mb-10 flex justify-center">
         <div className="rounded shadow-2xl w-11/12 sm:w-4/6">
           <div className="bg-primary text-white text-xl font-bold text-center p-2 rounded-t">
-            أشارك تجربتي
+          تواصلو معنا
           </div>
           <Formik
             initialValues={{
@@ -37,41 +43,33 @@ export default function ContactUs(): JSX.Element {
               message: "",
             }}
             validationSchema={ContactSchema}
-            onSubmit={async (values) => {
-              // ask({
-              //   variables: {
-              //     email: values.email,
-              //     email: values.email,
-              //     elected: "",
-              //     userName: values.fullname,
-              //     message: values.message,
-              //     xp: true,
-              //   },
-              // });
-
+            onSubmit={async (values, { resetForm }) => {
               
                 // Enable loading effect
+                // setIsSubmitting(true);
                 try {
+                  // send mail
                   await sendContactForm(values);
+
+                  // reset fields
+                  resetForm();
+
                   // stop loading effect
 
                   toast('تم إرسال بنجاح', { hideProgressBar: true, autoClose: 2000, type: 'success' })
                 } catch (error: any) {
                   // stop loading effect
+                  toast('فشل الارسال', { hideProgressBar: true, autoClose: 2000, type: 'error' })
                   console.log(error.message)
                 }
+
+                // setIsSubmitting(false);
+
               
 
             }}
           >
-            {({ errors, touched }) => (
-              <>
-                <div
-                  className="bg-green-400 p-2 rounded mx-3 text-white text-center "
-                  style={null ? {} : { display: "none" }}
-                >
-                  تم إرسال بنجاح
-                </div>
+            {({ errors, touched, isSubmitting }) => (
                 <Form className="mt-10 p-5 grid grid-cols-1 sm:grid-cols-2 gap-5">
                   {/* Full name */}
                   <div className="py-2">
@@ -147,16 +145,26 @@ export default function ContactUs(): JSX.Element {
                     ) : null}
                   </div>
                   
-                  <button
+                  {/* Submit button */}
+                  {
+                    isSubmitting ?
+                    <button
+                    disabled={isSubmitting}
                     type="submit"
-                    className="mt-3 sm:col-span-2 text-lg font-semibold
-                bg-primary w-full text-white rounded-lg
-                px-6 py-3 block shadow-xl hover:text-white hover:bg-primaryDarker"
+                    className={"mt-3 sm:col-span-2 text-lg font-semibold bg-gray-400 w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white"}
                   >
                     إرسال
                   </button>
+                  : 
+                  <button
+                    disabled={isSubmitting}
+                    type="submit"
+                    className={"mt-3 sm:col-span-2 text-lg font-semibold bg-primary w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-primaryDarker"}
+                  >
+                    إرسال
+                  </button>
+                  }
                 </Form>
-              </>
             )}
           </Formik>
         </div>
