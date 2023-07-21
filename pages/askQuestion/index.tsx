fe21ed91352648b49d7246e7eac5b32e3c1fe5f8
ismail hamdach <ismailhamdach@gmail.com>
2023-07-21 22:23:26 +0100

    Add askQuestion functioin

--- pages/askQuestion/index.tsx
+++ pages/askQuestion/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 import { ASK } from "../../graphql/askQuestion/queries";
 import { initializeApollo } from "../../apolloClient";
 import { useQuery, useMutation } from "@apollo/client";
+import { sendAskQuestionForm, sendContactForm } from "../../lib/api";
+import { toast } from "react-toastify";
 
 export default function AskQuestion(): JSX.Element {
   const ContactSchema = Yup.object().shape({
@@ -36,30 +38,34 @@ export default function AskQuestion(): JSX.Element {
               message: "",
             }}
             validationSchema={ContactSchema}
-            onSubmit={async (values) => {
-              ask({
-                variables: {
-                  object: values.object,
-                  elected: values.elected,
-                  email: values.email,
-                  userName: values.fullname,
-                  message: values.message,
-                  xp: false,
-                },
-              });
+            onSubmit={async (values, {resetForm}) => {
+              // Enable loading effect
+                // setIsSubmitting(true);
+                try {
+                  // send mail
+                  await sendAskQuestionForm(values);
+
+                  // reset fields
+                  resetForm();
+
+                  // stop loading effect
+
+                  toast('تم إرسال بنجاح', { hideProgressBar: false, autoClose: 2000, type: 'success' })
+                } catch (error: any) {
+                  // stop loading effect
+                  toast('فشل الارسال', { hideProgressBar: false, autoClose: 2000, type: 'error' })
+                  console.log(error.message)
+                }
+
+                // setIsSubmitting(false);
             }}
           >
-            {({ errors, touched }) => (
+            {({ errors, touched, isSubmitting }) => (
               <>
-                <div
-                  className="bg-green-400 p-2 rounded mx-3 text-white text-center"
-                  style={data ? {} : { display: "none" }}
-                >
-                  تم إرسال السؤال بنجاح
-                </div>
                 <Form className="mt-10 p-5 grid grid-cols-1 sm:grid-cols-2 gap-5">
+                  {/* Subject */}
                   <div className="py-2">
-                    <span className="px-1 text-sm text-gray-600">الموضوع</span>
+                    <span className="px-1 text-sm text-gray-600">الموضوع : (*)</span>
                     <Field
                       name="object"
                       type="text"
@@ -70,18 +76,12 @@ export default function AskQuestion(): JSX.Element {
                       <div className="text-red-600 ">{errors.object}</div>
                     ) : null}
                   </div>
+                  {/* Recever */}
                   <div className="py-2">
                     <span className="px-1 text-sm text-gray-600">
-                      حدد المعني بالسؤال
+                      حدد المعني بالسؤال : (*)
                     </span>
-                    {/* <Field
-                    name="elected"
-                    as="select"
-                    className="text-md block px-3 py-2  rounded-lg w-full
-                bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-primary focus:outline-none"
-                  >
-                    <option value={"aymen"}>aymen</option>;
-                  </Field> */}
+                    
                     <Field
                       name="elected"
                       type="text"
@@ -92,9 +92,10 @@ export default function AskQuestion(): JSX.Element {
                       <div className="text-red-600 ">{errors.elected}</div>
                     ) : null}
                   </div>
+                  {/* Full name */}
                   <div className="py-2">
                     <span className="px-1 text-sm text-gray-600">
-                      الإسم الكامل
+                      الإسم الكامل : (*)
                     </span>
                     <Field
                       name="fullname"
@@ -106,9 +107,10 @@ export default function AskQuestion(): JSX.Element {
                       <div className="text-red-600 ">{errors.fullname}</div>
                     ) : null}
                   </div>
+                  {/* Email */}
                   <div className="py-2">
                     <span className="px-1 text-sm text-gray-600">
-                      البريد الإلكتروني
+                      البريد الإلكتروني : (*)
                     </span>
                     <Field
                       name="email"
@@ -120,8 +122,9 @@ export default function AskQuestion(): JSX.Element {
                       <div className="text-red-600 ">{errors.email}</div>
                     ) : null}
                   </div>
+                  {/* Message */}
                   <div className="py-2 sm:col-span-2">
-                    <span className="px-1 text-sm text-gray-600">الرسالة </span>
+                    <span className="px-1 text-sm text-gray-600">الرسالة : (*)</span>
                     <Field
                       name="message"
                       component="textarea"
@@ -132,15 +135,28 @@ export default function AskQuestion(): JSX.Element {
                       <div className="text-red-600 ">{errors.message}</div>
                     ) : null}
                   </div>
-                  <button
-                    type="submit"
-                    className="mt-3  sm:col-span-2 text-lg font-semibold
-                bg-primary w-full text-white rounded-lg
-                px-6 py-3  shadow-xl hover:text-white hover:bg-primaryDarker"
-                    disabled={loading}
-                  >
-                    إرسال
-                  </button>
+                  {
+                    isSubmitting ? 
+                      <div
+                        className="mt-3 sm:col-span-2 text-lg font-semibold
+                            h-14 w-full text-white rounded-lg
+                            px-6 py-3  bg-[url('/images/Ellipsis-2.1s-200px.gif')] bg-no-repeat bg-center"
+                      >
+                        
+                      </div>
+                      :
+                      <button
+                        type="submit"
+                        className="mt-3  sm:col-span-2 text-lg font-semibold
+                            bg-primary w-full text-white rounded-lg
+                            px-6 py-3  shadow-xl hover:text-white hover:bg-primaryDarker"
+                        disabled={loading}
+                      >
+                        إرسال
+                      </button>
+
+                  }
+
                 </Form>
               </>
             )}
