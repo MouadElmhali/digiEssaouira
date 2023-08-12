810b083218a553781dcc4eeaf901001423f8eaf5
ismail hamdach <ismailhamdach@gmail.com>
2023-08-12 17:50:12 +0100

    Fix graduateds responsivity

--- pages/graduates/index.tsx
+++ pages/graduates/index.tsx
@@ -10,55 +10,66 @@ import { IGetCoursesData } from "../../graphql/courses/types";
 import Link from "next/link";
 import Image from "next/image";
 
-
 export async function getServerSideProps() {
-    const client = initializeApollo();
-    const {
-        data: { getGraduates
-        },
-    } = await client.query({
-        query: GET_GRADUATES,
-    });
+  const client = initializeApollo();
+  const {
+    data: { getGraduates },
+  } = await client.query({
+    query: GET_GRADUATES,
+  });
 
-    return {
-        props: {
-            getGraduates
-        },
-    };
+  return {
+    props: {
+      getGraduates,
+    },
+  };
 }
 
 export default function Graduates({
-    getGraduates
+  getGraduates,
 }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
-    return (
-        <>
-            <Head>
-                <title>DigiEssaouira |  خريجي</title>
-            </Head>
-            <main>
-                <Section>
-                    <h1 className="text-2xl font-semibold text-center text-primaryDarker mt-36 mb-10">خريجي DIGIESSAOUIRA</h1>
-                    <div className="grid grid-cols-1 sm:grid-cols-4 gap-5 ">
-                        {getGraduates.map(({ id, post, name, pictureUrl }) => (
-                            <div key={id} className="min-w-[180px] rounded overflow-hidden">
-                                <Link href={{
-                                    pathname: `${routes.graduates.path}/${id}`,
-                                }}>
-                                    <a className="flex flex-col gap-4 flex-1 h-full">
-                                    <Image height={400} width={400} className="object-fill bg-white  bg-[url('/images/loading.gif')] bg-cover bg-no-repeat bg-center" alt="" src={`/images/graduates/${pictureUrl}`} 
-                                    />
+  return (
+    <>
+      <Head>
+        <title>DigiEssaouira | خريجي</title>
+      </Head>
+      <main>
+        <Section className="mx-8 md:mx-auto">
+          <h1 className="text-2xl font-semibold text-center text-primaryDarker mt-20 md:mt-36 mb-10">
+            خريجي DIGIESSAOUIRA
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-5 gap-y-8 ">
+            {getGraduates.map(({ id, post, name, pictureUrl }: any) => (
+              <div
+                key={id}
+                className="max-w-[15rem] rounded-lg overflow-hidden shadow-lg transition ease-in-out duration-200 hover:scale-105"
+              >
+                <Link
+                  href={{
+                    pathname: `${routes.graduates.path}/${id}`,
+                  }}
+                >
+                  <a className="flex flex-col h-full">
+                    <Image
+                      height={400}
+                      width={400}
+                      className="bg-white  bg-[url('/images/loading.gif')] bg-cover bg-no-repeat bg-center"
+                      alt={name}
+                      src={`/images/graduates/${pictureUrl}`}
+                      objectFit="contain"
+                    />
 
-                                        <div className="bg-primary text-white font-bold text-lg text-center py-5 px-3 rounded overflow-hidden">
-                                            {name} <br />
-                                            {post}
-                                        </div>
-                                    </a>
-                                </Link>
-                            </div>
-                        ))}
+                    <div className="bg-primary text-white font-bold text-center py-3 overflow-hidden">
+                      {name} <br />
+                      {post}
                     </div>
-                </Section>
-            </main >
-        </>
-    );
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </Section>
+      </main>
+    </>
+  );
 }
