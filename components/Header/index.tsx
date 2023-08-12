950bfc07ce5ee36d03cfd84bce3ce4983eb18d62
ismail hamdach <ismailhamdach@gmail.com>
2023-08-12 17:21:18 +0100

    Fix resources responsivity and animation

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -20,7 +20,7 @@ export default function Header({
     styles ?
       (
         <header
-          className={` w-full  md:h-[80vh] sm:h-[65vh] bg-center bg-cover bg-no-repeat bg-center relative z-0 sm:right-1/2 sm:translate-x-1/2  sm:mt-40 after:content-[''] after:absolute  after:h-full after:w-full after:left-0 after:top-0 after:-z-[1] ${styles}  `}
+          className={` w-full  md:h-[80vh] sm:h-[65vh] bg-center bg-cover bg-no-repeat relative z-0 sm:right-1/2 sm:translate-x-1/2  sm:mt-40 after:content-[''] after:absolute  after:h-full after:w-full after:left-0 after:top-0 after:-z-[1] ${styles}  `}
         >
           <div className="text-white absolute  top-1/2 -translate-y-1/2  right-1/2 translate-x-1/2 flex flex-col gap-14 items-center w-11/12 [&>p:first-child]:text-center">
             {children}
@@ -30,9 +30,9 @@ export default function Header({
       :
       (
         <header
-        className="flex flex-wrap md:flex-nowrap items-center justify-center px-10 mt-36 gap-10"
+        className="flex flex-wrap md:flex-nowrap items-center justify-center px-10 mt-48 gap-10"
         >
-              <h1 className="text-4xl font-bold mb-4 text-center text-primary sm:text-center">{title}</h1>
+              <h1 className="text-4xl font-bold text-center text-primary sm:text-center">{title}</h1>
               <img className="w-full h-auto max-w-md" src={image} alt="Placeholder image"/>
         </header>
       )
